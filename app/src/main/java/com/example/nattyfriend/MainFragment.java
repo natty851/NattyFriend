f2ddf9558b9934a7815d973202b4b9fde12687fb
natty851 <saisarlee_nat@hotmail.com>
2019-02-23 15:56:15 +0700

    after lunch

--- app/src/main/java/com/example/nattyfriend/MainFragment.java
+++ app/src/main/java/com/example/nattyfriend/MainFragment.java
@@ -2,10 +2,12 @@ package com.example.nattyfriend;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -18,6 +20,20 @@ public class MainFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+//        Register Controller
+        TextView textView = getView().findViewById(R.id.txtRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Replace Fragment
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.contentMainFragment,new RegisterFragment()).addToBackStack(null).commit();
+            }
+        });
+
+    }   //Main Method
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -26,4 +42,4 @@ public class MainFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 
-}
+} //Main Class
--- app/src/main/java/com/example/nattyfriend/RegisterFragment.java
+++ app/src/main/java/com/example/nattyfriend/RegisterFragment.java
@@ -2,7 +2,9 @@ package com.example.nattyfriend;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +20,26 @@ public class RegisterFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toolbar
+        Toolbar toolbar = getView().findViewById(R.id.toolbar);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity)getActivity()).getSupportActionBar().setTitle("Register");
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+
+
+    }   //Main Method
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
