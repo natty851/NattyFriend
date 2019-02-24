e47ec37eb81349bba92012c64502a5bbc21cedf7
natty851 <saisarlee_nat@hotmail.com>
2019-02-24 11:53:10 +0700

    at 24 morning

---
+++ app/src/main/java/com/example/nattyfriend/MyAlert.java
@@ -0,0 +1,30 @@
+package com.example.nattyfriend;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+
+public class MyAlert {
+
+    private Context context;
+
+    public MyAlert(Context context) {
+        this.context = context;
+    }
+
+    public void  normalDialog(String title, String message){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+
+
+    }
+}
--- app/src/main/java/com/example/nattyfriend/RegisterFragment.java
+++ app/src/main/java/com/example/nattyfriend/RegisterFragment.java
@@ -1,19 +1,32 @@
 package com.example.nattyfriend;
 
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class RegisterFragment extends Fragment {
+    //
+    private ImageView imageView;
+    private boolean aBoolean = true;
+    private Uri uri;
 
 
     public RegisterFragment() {
@@ -24,10 +37,89 @@ public class RegisterFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-//        Create Toolbar
+        //        Create Toolbar
+        createToolbar();
+        //  Avatar Controller
+        chooseImage();
+
+
+    }   //Main Method
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == getActivity().RESULT_OK) {
+            uri = data.getData();
+            aBoolean = false;
+            try {
+
+                Bitmap bitmap = BitmapFactory.decodeStream(getActivity().getContentResolver().openInputStream(uri));
+                Bitmap bitmap1 = Bitmap.createScaledBitmap(bitmap, 800, 600, false);
+                imageView.setImageBitmap(bitmap1);
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        } //if
+
+    }
+
+    private void chooseImage() {
+        imageView = getView().findViewById(R.id.imAvatar);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "Please Choose Apps"), 1);
+
+            }
+        });
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_register, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.itemUpload) {
+
+            checkValue();
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void checkValue() {
+        MyAlert myAlert = new MyAlert(getActivity());
+        EditText nameEditText = getView().findViewById(R.id.edtName);
+        EditText userEditText = getView().findViewById(R.id.edtUser);
+        EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+
+        String name = nameEditText.getText().toString().trim();
+        String user = userEditText.getText().toString().trim();
+        String password = passwordEditText.getText().toString().trim();
+
+        if (aBoolean) {
+            // None Choose Image
+            myAlert.normalDialog("None Choose Image", "Please Choose Avatar");
+
+        }
+    } //CheckValue
+
+    private void createToolbar() {
         Toolbar toolbar = getView().findViewById(R.id.toolbar);
-        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
-        ((MainActivity)getActivity()).getSupportActionBar().setTitle("Register");
+        ((MainActivity) getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle("Register");
+        ((MainActivity) getActivity()).getSupportActionBar().setSubtitle("Please Fill Every Fields");
         ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
         ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
@@ -36,10 +128,8 @@ public class RegisterFragment extends Fragment {
                 getActivity().getSupportFragmentManager().popBackStack();
             }
         });
-
-
-
-    }   //Main Method
+        setHasOptionsMenu(true);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
